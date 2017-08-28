ca4e3f5f2275a0b4ed5f424e7021c9f3d272af9c
Sergey Toropov <toropov.si@dvfu.ru>
2017-08-28 12:33:01 +1000

    Решено несколько задач.

---
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2409/Item.java
@@ -0,0 +1,11 @@
+package com.javarush.task.task24.task2409;
+
+/**
+ * @author sergeytoropov
+ * @since 28.08.17
+ */
+public interface Item {
+    int getId();
+    double getPrice();
+    String getTM();
+}
---
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2409/Jeans.java
@@ -0,0 +1,10 @@
+package com.javarush.task.task24.task2409;
+
+/**
+ * @author sergeytoropov
+ * @since 28.08.17
+ */
+public interface Jeans extends Item {
+    int getLength();
+    int getSize();
+}
